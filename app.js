1d341bd16841ddd0de0b0f51cd742ccc9e81444a
DizZer0 <hamistry@yandex.ru>
2023-02-05 16:56:13 +0300

    добавил фронт в whitelist

--- app.js
+++ app.js
@@ -12,11 +12,15 @@ const NoDataFound = require('./errors/NoDataFound');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { limiter, moviesdb } = require('./utils/config')
 
+const corsOptions = {
+  origin: 'http://dizzero.diplom.nomoredomainsclub.ru/',
+};
+
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.use(requestLogger);
 
