8d78a28ac042559388552b65ce5980c72cc0ff23
DizZer0 <hamistry@yandex.ru>
2023-02-05 17:25:58 +0300

    q

--- app.js
+++ app.js
@@ -12,16 +12,11 @@ const NoDataFound = require('./errors/NoDataFound');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { limiter, moviesdb } = require('./utils/config')
 
-const corsOptions = {
-  origin: 'http://dizzero.diplom.nomoredomainsclub.ru',
-  optionsSuccessStatus: 200,
-};
-
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(requestLogger);
 
