16c161c8a436e3048d7c7d28c266c8ebcc4ec889
DizZer0 <hamistry@yandex.ru>
2023-02-05 17:18:47 +0300

    g

--- app.js
+++ app.js
@@ -5,15 +5,23 @@ const express = require('express');
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
+const cors = require('cors');
 
 const errHandler = require('./middlewares/errHandler');
 const NoDataFound = require('./errors/NoDataFound');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { limiter, moviesdb } = require('./utils/config')
 
+const corsOptions = {
+  origin: 'dizzero.diplom.nomoredomainsclub.ru',
+  optionsSuccessStatus: 200,
+};
+
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+app.use(cors(corsOptions));
 
 app.use(requestLogger);
 
